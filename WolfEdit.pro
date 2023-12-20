ebeeef914a56ce02e828d377a461e91a72dcb547
P.J. Finlay <pjappdevelopment@gmail.com>
2023-12-20 09:05:00 -0600

    Cleaned up starter files
    - Removed mainwindow.*
    - Create new starter app using ChatGPT-3.5

--- WolfEdit.pro
+++ WolfEdit.pro
@@ -9,14 +9,7 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    main.cpp \
-    mainwindow.cpp
-
-HEADERS += \
-    mainwindow.h
-
-FORMS += \
-    mainwindow.ui
+    main.cpp
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QMainWindow>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    // Create the main window
+    QMainWindow mainWindow;
+
+    // Create a central widget (e.g., QTextEdit)
+    QTextEdit *textEdit = new QTextEdit(&mainWindow);
+
+    // Create a button
+    QPushButton *button = new QPushButton("Click me", &mainWindow);
+
+    // Create a vertical layout and add the widgets
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(textEdit);
+    layout->addWidget(button);
+
+    // Set the layout for the central widget
+    QWidget *centralWidget = new QWidget(&mainWindow);
+    centralWidget->setLayout(layout);
+
+    // Set the central widget for the main window
+    mainWindow.setCentralWidget(centralWidget);
+
+    // Connect button click signal to a custom slot or function
+    // QObject::connect(button, SIGNAL(clicked()), yourCustomSlotOrFunction);
+
+    // Set the main window properties
+    mainWindow.setWindowTitle("WolfEdit");
+    mainWindow.show();
+
     return a.exec();
 }
+
