089603b6c5eab516fdc5232daadba5501958191b
P.J. Finlay <pjappdevelopment@gmail.com>
2023-12-22 08:42:57 -0600

    Launch WolfEdit from Python

---
+++ wolfedit.py
@@ -0,0 +1,12 @@
+import subprocess
+import pathlib
+
+EXEC_PATH = pathlib.Path(__file__).parent.absolute() / "build" / "WolfEdit"
+
+
+def launch_wolfedit():
+    subprocess.Popen([EXEC_PATH])
+
+
+if __name__ == "__main__":
+    launch_wolfedit()
