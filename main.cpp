b8e3ec483b6d292a14e9b262b90e8aeb922c9e3c
P.J. Finlay <pjappdevelopment@gmail.com>
2023-12-22 08:55:34 -0600

    Add support for opening multiple tabs

--- main.cpp
+++ main.cpp
@@ -7,19 +7,24 @@
 #include <QFileDialog>
 #include <QTextStream>
 #include <QString>
+#include <QTabWidget>
 
 class TextEditor : public QMainWindow {
     Q_OBJECT
 
 public:
     TextEditor(QWidget *parent = nullptr) : QMainWindow(parent) {
-        textEdit = new QTextEdit(this);
-        setCentralWidget(textEdit);
+        tabWidget = new QTabWidget(this);
+        setCentralWidget(tabWidget);
 
         createMenu();
 
         setWindowTitle("WolfEdit");
         resize(800, 600);
+    }
+
+    QTabWidget* getTabWidget() const {
+        return tabWidget;
     }
 
 private slots:
@@ -28,27 +33,35 @@ private slots:
         if (!fileName.isEmpty()) {
             QFile file(fileName);
             if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-                QTextStream in(&file);
-                textEdit->setPlainText(in.readAll());
+                QTextEdit *textEdit = new QTextEdit(this);
+                textEdit->setPlainText(QString::fromUtf8(file.readAll()));
                 file.close();
+
+                int tabIndex = tabWidget->addTab(textEdit, QFileInfo(fileName).fileName());
+                tabWidget->setCurrentIndex(tabIndex);
             }
         }
     }
 
     void saveFile() {
-        QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt);;All Files (*)"));
-        if (!fileName.isEmpty()) {
-            QFile file(fileName);
-            if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-                QTextStream out(&file);
-                out << textEdit->toPlainText();
-                file.close();
+        if (tabWidget->count() > 0) {
+            QTextEdit *currentTextEdit = qobject_cast<QTextEdit*>(tabWidget->currentWidget());
+            if (currentTextEdit) {
+                QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt);;All Files (*)"));
+                if (!fileName.isEmpty()) {
+                    QFile file(fileName);
+                    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                        QTextStream out(&file);
+                        out << currentTextEdit->toPlainText();
+                        file.close();
+                    }
+                }
             }
         }
     }
 
 private:
-    QTextEdit *textEdit;
+    QTabWidget *tabWidget;
 
     void createMenu() {
         QMenu *fileMenu = menuBar()->addMenu(tr("File"));
@@ -67,9 +80,39 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     TextEditor editor;
+
+    // Check if command line arguments are provided
+    bool initialFilePathProvided  = false;
+    if (argc > 1) {
+        // Attempt to open the file specified in the command line argument
+        QString fileName = argv[1];
+        QFile file(fileName);
+        if (file.exists()) {
+            initialFilePathProvided = true;
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                QTextEdit *textEdit = new QTextEdit(&editor);
+                textEdit->setPlainText(QString::fromUtf8(file.readAll()));
+                file.close();
+
+                // Add the initial tab with the content of the specified file
+                int tabIndex = editor.getTabWidget()->addTab(textEdit, QFileInfo(fileName).fileName());
+                editor.getTabWidget()->setCurrentIndex(tabIndex);
+            }
+        }
+    }
+
+    // If no initial file path was provided, add an empty tab
+    if (!initialFilePathProvided) {
+        QTextEdit *textEdit = new QTextEdit(&editor);
+        int tabIndex = editor.getTabWidget()->addTab(textEdit, "Untitled");
+        editor.getTabWidget()->setCurrentIndex(tabIndex);
+    }
+
     editor.show();
 
     return app.exec();
 }
 
+
 #include "main.moc"
+
