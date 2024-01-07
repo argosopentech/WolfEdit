f3b4000985e6204270682d7389865bd185d953d8
P.J. Finlay <pjappdevelopment@gmail.com>
2024-01-07 14:46:46 -0600

    Improved Debian packaging

--- scripts/package_deb.sh
+++ scripts/package_deb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p build/deb/usr/bin
-cp -r DEBIAN build/deb/
-cp build/WolfEdit build/deb/usr/bin/
+mkdir -p build/debian_build/WolfEdit/usr/bin
+cp -r DEBIAN build/debian_build/WolfEdit
+cp build/WolfEdit build/debian_build/WolfEdit/usr/bin/
 cd build/
-dpkg-deb --build deb
+dpkg-deb --build debian_build/WolfEdit
 
